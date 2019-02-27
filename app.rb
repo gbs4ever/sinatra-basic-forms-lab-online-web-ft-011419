a20684d9416fea983e7c3b7a012cb9cef33534ed
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-27 20:46:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @puppy = puppy.new(pramas['name'],pramas['breed'],pramas['age'])
+    @puppy = puppy.new(params['name'],params['breed'],params['age'])
 
 
   erb :display_puppy
